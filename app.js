287ea875e3262d85f24647a1fbaa5edadfd77bb2
Ahmed Tawfik <ahmedtawfik833@gmail.com>
2024-04-16 04:45:47 +0200

    implemented cors

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 /////// Global Middlewares
 app.use(cors());
+app.options('*', cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
